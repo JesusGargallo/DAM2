192a38a5d9068fcd64534523f3456837b3a9b99d
Alumne <Alumne@AI104-PC10>
2020-12-11 19:17:48 +0100

    comentarios hechos

--- M9/UF02/Ac1_M9_UF2/src/ac1_m9_uf2/Ac1_M9_UF2.java
+++ M9/UF02/Ac1_M9_UF2/src/ac1_m9_uf2/Ac1_M9_UF2.java
@@ -8,11 +8,13 @@ public class Ac1_M9_UF2 {
 		private int operador1;
 		private int operador2;
 		
+                //constructor de la clase Sumar
 		public Sumar(int operador1, int operador2) {
 			this.operador1 = operador1;
 			this.operador2 = operador2;
 			}
 			
+                //metodo para hacer la suma
 		@Override
 		public Integer call() throws Exception {
 			return operador1 + operador2;
@@ -21,17 +23,29 @@ public class Ac1_M9_UF2 {
 
 	public static void main(String[] args) throws
 		InterruptedException, ExecutionException {
+                        
+                        //executara 5 files
 			ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(5);
+                        
+                        //creas un arraylist
 			List<Sumar> llistaTasques= new ArrayList<Sumar>();
-			for (int i = 0; i < 10; i++) {
+                        
+                        //lanzara 25 sumas aleatoria con numeros aleatorios
+			for (int i = 0; i < 25  ; i++) {
 				Sumar calcula = new Sumar((int)(Math.random()*10), (int)(Math.random()*10));
 				llistaTasques.add(calcula);
 				}
+                        
+                        //crea una array de lista para ejecutar la lista con las tascas
+                        //donde se han guardado las sumas
 			List <Future<Integer>> llistaResultats;
 			llistaResultats = executor.invokeAll(llistaTasques);
 			
+                        
+                        //espara a que acaben los hilos
 			executor.shutdown();
 			
+                        //mostrar los resultados
 			for (int i = 0; i < llistaResultats.size(); i++) {
 				Future<Integer> resultat = llistaResultats.get(i);
 				try {
