82c719484941577de605384c3abce0c0e0568a9b
=Jesus Gargallo <=jesus.gargallo@insbaixcamp.cat>
2021-01-19 20:02:55 +0100

    subida

---
+++ M6/UF02/exercicio5/ex5_Damas/src/ex5_damas/Ex5_Damas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex5_damas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Ex5_Damas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
