c1cd44bb4bc6218b8c519c3b4a2df43e60ead4fd
Alumne <Alumne@Ai104-PC09>
2021-02-11 20:07:29 +0100

    exemple 1

---
+++ M3/Exemple1/exemple1/src/exemple1/Exemple1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemple1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Alumne
+ */
+public class Exemple1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Test Frame 1");
+        frame.setSize(200,100);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
