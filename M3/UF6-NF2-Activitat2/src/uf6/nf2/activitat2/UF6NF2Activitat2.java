33dc3c84622e6dba4fd7d7b4f171ecd93214e78a
Alumne <Alumne@Ai104-PC09>
2021-03-17 21:00:19 +0100

    commit act 2 no avaluable m3

---
+++ M3/UF6-NF2-Activitat2/src/uf6/nf2/activitat2/UF6NF2Activitat2.java
@@ -0,0 +1,28 @@
+package uf6.nf2.activitat2;
+
+import java.util.Scanner;
+
+
+public class UF6NF2Activitat2 {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);    
+    
+    calcul(1,13);
+    }
+    
+    public static void calcul (int fila, int pedres){
+        if (fila == pedres){
+            System.out.println(fila + ", " + 0);
+            
+        } else if (fila > pedres){
+            System.out.println((fila -1) + ", " + pedres);
+            
+        }else if (fila < pedres){
+            calcul(fila + 1, pedres - fila);
+        }
+    }
+    
+}
+
+
