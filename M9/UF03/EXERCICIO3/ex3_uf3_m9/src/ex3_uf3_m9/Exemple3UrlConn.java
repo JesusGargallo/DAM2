32ff596f875422545a379ec8e2ac4c242a3eed45
=Jesus Gargallo <=jesus.gargallo@insbaixcamp.cat>
2021-03-21 22:22:37 +0100

    act3 m9

--- M9/UF03/EXERCICIO3/ex3_uf3_m9/src/ex3_uf3_m9/Exemple3UrlConn.java
+++ M9/UF03/EXERCICIO3/ex3_uf3_m9/src/ex3_uf3_m9/Exemple3UrlConn.java
@@ -8,52 +8,37 @@ public class Exemple3UrlConn {
 	
 	@SuppressWarnings("rawtypes")
 	public static void main (String[] args) {
-		
-		try {
-			String cadena;
-			URL url = new URL("http://insbaixcamp.org/");
-			URLConnection connexio = url.openConnection();
-			
-			System.out.println("===============================================================");
-			System.out.println("ADREÇA, DATA I CONTINGUT");
-			System.out.println("Adreça [getURL]: " + connexio.getURL());
-			
-			Date data = new Date(connexio.getLastModified());
-			System.out.println("Data última modificació [getLastModified()]: " + data);
-			System.out.println("Tipus de Contingut [getContentType()]" + connexio.getContentType());
-			
-			System.out.println("===============================================================");
-			System.out.println("TOTS ELS CAMPS DE CAPÇALERA AMB getHeaderFields(): ");
-			
-			//Fem servir una estructura Map per a recuperar capçaleres
-			Map campsCapçalera = connexio.getHeaderFields();
-			Iterator it = campsCapçalera.entrySet().iterator();
-			
-			while (it.hasNext()) {
-				Map.Entry map = (Map.Entry) it.next();
-				System.out.println(map.getKey() + ":" + map.getValue());
-			}
-			
-			System.out.println("===============================================================");
-			System.out.println("Camps 1 2 3 4 i 5 de Capçalera");
-			System.out.println("getHeaderField(1)=> " + connexio.getHeaderField(1));
-                        System.out.println("getHeaderField(2)=> " + connexio.getHeaderField(2));
-                        System.out.println("getHeaderField(3)=> " + connexio.getHeaderField(3));
-			System.out.println("getHeaderField(4)=> " + connexio.getHeaderField(4));
-                        System.out.println("getHeaderField(5)=> " + connexio.getHeaderField(5));
-			System.out.println("===============================================================");
-			
-			System.out.println("Contingut de [url.getFile()]: " + url.getFile());
-			BufferedReader pagina = new BufferedReader(new InputStreamReader(url.openStream()));
-			
-			while ((cadena = pagina.readLine()) != null) {
-				
-				System.out.println(cadena);
-			}
-			
-		}
-		catch (MalformedURLException e) { e.printStackTrace();}
-		catch (IOException e) {e.printStackTrace();}
+            
+            Scanner sc = new Scanner(System.in);
+            String c;
+
+            try {
+                URL url = new URL(args[0]);
+                URLConnection conn = url.openConnection();
+                
+                System.out.println("Los campos de la capçelera");
+                Map camps = conn.getHeaderFields();
+                Iterator iterator = camps.entrySet().iterator();
+
+                while (iterator.hasNext()) {
+                    Map.Entry map = (Map.Entry) iterator.next();
+                    System.out.println(map.getKey() + " con el valor " + map.getValue());
+                }
+
+                for (int i = 1; i <= Integer.valueOf(args[1]); i++) {
+                    System.out.println(i + " " + conn.getHeaderField(i));
+                }
+
+                BufferedReader pagina
+                        = new BufferedReader(new InputStreamReader(url.openStream()));
+
+                while ((c = pagina.readLine()) != null) {
+                    if (c.contains(args[2])) System.out.println(c);
+                }
+
+            } catch (IOException | NumberFormatException e) {
+                System.out.println(e);
+            }
 	}
 
 }
