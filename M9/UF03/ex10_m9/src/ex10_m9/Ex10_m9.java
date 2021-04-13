53940299204acf2c38fe2e083a3ebf1b88ecdc53
Alumne <Alumne@Ai104-PC09>
2021-04-13 19:29:04 +0200

    empezando la act 10 de m9

---
+++ M9/UF03/ex10_m9/src/ex10_m9/Ex10_m9.java
@@ -0,0 +1,9 @@
+package ex10_m9;
+
+public class Ex10_m9 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ M9/UF03/ex10_m9/src/ex10_m9/ServidorTCP2.java
@@ -0,0 +1,93 @@
+package ex10_m9;
+
+import java.net.*;
+import java.util.Scanner;
+import java.io.*;
+
+public class ServidorTCP2 implements Runnable {
+
+	Socket client;
+	ServerSocket server;
+	static int numClient;
+	String cadena = "";
+
+	public ServidorTCP2(Socket clientConnectat, ServerSocket server) {
+		this.client = clientConnectat;
+		this.server = server;
+		this.numClient ++;
+	}
+        public static void main (String[] args) throws IOException {
+
+		// **********SEGONA PART**********
+		Scanner teclado = new Scanner(System.in);
+
+
+		int numPort = 60000;
+		ServerSocket servidor = new ServerSocket(numPort);
+
+
+
+		// Demanem el numClients per a indicar 
+		// el max de clients que rebra el servidor
+		System.out.println("Introdueix el num de clients que podra rebre el servidor: ");
+		int numClients = teclado.nextInt();
+
+
+		Runnable[] arrayRunnable = new Runnable[numClients];
+		Thread[] arrayThread = new Thread[numClients];
+
+		// Determinem les vegades que es conectaran els clients
+		for (int i = 0; i < arrayRunnable.length; i++) {
+
+
+			Socket clientConnectat = servidor.accept();
+
+			// Runnable
+			arrayRunnable[i] = new ServidorTCP2(clientConnectat, servidor);
+
+			// Thread
+			arrayThread[i] = new Thread(arrayRunnable[i]);
+			arrayThread[i].start();
+
+		}
+
+
+	}
+
+	@Override
+	public void run() {
+
+		try {
+			PrintWriter fsortida = null;
+			BufferedReader fentrada = null;
+
+			System.out.println("Esperant connexió... ");
+			System.out.println("Client " + this.numClient + " connectat... ");
+
+			//FLUX DE SORTIDA AL CLIENT
+			fsortida = new PrintWriter(this.client.getOutputStream(), true);
+
+
+			//FLUX D'ENTRADA DEL CLIENT
+			fentrada = new BufferedReader(new InputStreamReader(this.client.getInputStream()));
+
+
+			while ((cadena = fentrada.readLine()) != null) {
+
+				fsortida.println(cadena);
+				System.out.println("Rebent: "+cadena);
+				if (cadena.equals("*")) break;
+
+			}
+			fentrada.close();
+			fsortida.close();
+			this.client.close();
+			this.server.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
